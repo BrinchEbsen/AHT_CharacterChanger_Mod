f5c2bf888b6a6ebebc09aa1f178c87a9f18bf0fc
Ebbe <37877015+BrinchEbsen@users.noreply.github.com>
2026-02-06 17:08:22 +0100

    Removed some leftover code

--- include/gamedraw.h
+++ include/gamedraw.h
@@ -10,9 +10,4 @@ extern DrawLoop gamedraw_callback;
 
 void gamedraw_loop(void);
 
-void draw_3d_ellipse(void *pWnd, mat_44 *normalMtx, EXVector *centerPos,
-    float width, float height, RGBA color, int divisions);
-
-void draw_load_triggers(void* pWnd);
-
 #endif /* GAMEDRAW_H */
